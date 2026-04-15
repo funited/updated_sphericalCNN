f03813b404829e7a1f90590622f2417965214742
funited <48507831+funited@users.noreply.github.com>
2026-04-14 21:39:23 -0400

    Update so3_conv.py

--- s2cnn/SOFT/so3_conv.py
+++ s2cnn/SOFT/so3_conv.py
@@ -13,8 +13,8 @@ from torch.nn.parameter import Parameter
 from torch.nn.modules import Module
 
 from .so3_fft import SO3_fft_real, SO3_ifft_real
-from .so3_ft import so3_rft
-from .so3_mm  import so3_mm
+from s2cnn import so3_rft
+from s2cnn  import so3_mm
 
 
 class SO3Convolution(Module):
