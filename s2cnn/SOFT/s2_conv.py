2ea51be27c00ca5569e1c046f4a8ddcfe5758544
funited <48507831+funited@users.noreply.github.com>
2026-04-14 21:29:47 -0400

    Update s2_conv.py

--- s2cnn/SOFT/s2_conv.py
+++ s2cnn/SOFT/s2_conv.py
@@ -12,10 +12,10 @@ import torch
 from torch.nn.parameter import Parameter
 from torch.nn.modules import Module
 
-from s2_fft import S2_fft_real
-from so3_fft import SO3_ifft_real
-from s2_rft  import s2_rft
-from s2_mm   import s2_mm
+from .s2_fft import S2_fft_real
+from .so3_fft import SO3_ifft_real
+from .s2_rft  import s2_rft
+from .s2_mm   import s2_mm
 
 
 class S2Convolution(Module):
